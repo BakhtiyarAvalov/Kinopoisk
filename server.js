b442d7356b8961f52d50fa8489968cec2173f004
BakhtiyarAvalov <Avalov90@mail.ru>
2023-06-09 22:34:09 +0600

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static(__dirname + '/public'))
+app.set ("view engine", "ejs")
+
+app.get('/', (req, res) =>{
+    res.render("index.ejs")
+})
+
+app.get('/login', (req, res)=>{
+    res.render("login.ejs")
+})
+
+app.get('/register', (req, res)=>{
+    res.render("register.ejs")
+})
+
+const PORT = 8000;
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+})
