3603587853438b3762470f55f23b2db42e83acaf
BakhtiyarAvalov <Avalov90@mail.ru>
2023-06-20 21:56:57 +0600

    added entry and exit function

--- server/auth/controller.js
+++ server/auth/controller.js
@@ -29,11 +29,21 @@ const signUp = async(req, res)=>{
 };
 
 const signIn = (req, res) => {
-    res.redirect('/profile')
+    res.redirect(`/profile/${req.user._id}`)
+}
+
+const signOut = (req, res) => {
+    req.logout(function(err){
+        if(err){
+            console.log(err);
+        }
+    })
+    res.redirect('/')
 }
 
 module.exports = 
 {
     signUp, 
     signIn,
+    signOut
 }
--- server/pages/router.js
+++ server/pages/router.js
@@ -6,34 +6,34 @@ const Country = require('../Country/Country')
 router.get('/', async(req, res) =>{
     const allGenres = await Genres.find()
     const getAllCountries = await Country.find()
-    res.render("index.ejs" , {genres: allGenres, countries: getAllCountries})
+    res.render("index.ejs" , {genres: allGenres, user: req.user ? req.user : {}})
 })
 
 router.get('/login', (req, res)=>{
-    res.render("login.ejs")
+    res.render("login.ejs", {user: req.user ? req.user : {}})
 })
 
 router.get('/register', (req, res)=>{
-    res.render("register.ejs")
+    res.render("register.ejs", {user: req.user ? req.user : {}})
 })
 
-router.get('/profile', (req, res)=>{
-    res.render("profile.ejs")
+router.get('/profile/:id', (req, res)=>{
+    res.render("profile.ejs", {user: req.user ? req.user : {}})
 })
 
 router.get('/admin', (req, res)=>{
-    res.render("adminProfile.ejs")
+    res.render("adminProfile.ejs", {user: req.user ? req.user : {}})
 })
 
 router.get('/new', async(req, res)=>{
     const allGenres = await Genres.find()
     const getAllCountries = await Country.find()
-    res.render("newFilm.ejs", {genres : allGenres, countries: getAllCountries })
+    res.render("newFilm.ejs", {genres : allGenres, countries: getAllCountries, user: req.user ? req.user : {} })
 })
 
 router.get('/edit', async(req, res)=>{
     const allGenres = await Genres.find()
     const getAllCountries = await Country.find()
-    res.render("editFilm.ejs", {genres : allGenres, countries: getAllCountries})
+    res.render("editFilm.ejs", {genres : allGenres, countries: getAllCountries, user: req.user ? req.user : {}})
 })
 module.exports = router
