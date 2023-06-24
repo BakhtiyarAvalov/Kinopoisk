6ea26de63689c269779a2609ae1f21f0f356a397
BakhtiyarAvalov <Avalov90@mail.ru>
2023-06-24 19:40:27 +0600

    modernized the login function for the user

--- server/auth/controller.js
+++ server/auth/controller.js
@@ -1,33 +1,36 @@
 const User = require('./user');
 const bcrypt = require('bcrypt')
 
-const signUp = async(req, res)=>{
-    if( 
-        req.body.email.lenght <= 0 &&
-        req.body.full_name.lenght <= 0 &&
-        req.body.password.lenght <= 0 &&
-        req.body.re_password.lenght <= 0
+const signUp = async (req, res) => {
+    if(
+        req.body.email.length > 0 &&
+        req.body.full_name.length > 0 &&
+        req.body.password.length > 0 &&
+        req.body.re_password.length > 0
     ){
+        const findUser = await User.findOne({ email: req.body.email }).count()
+        if (findUser) {
+            res.redirect('/register?error=3')
+        }
+        else if(req.body.password !== req.body.re_password){
+            res.redirect('/register?error=2')
+        }else{
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(req.body.password, salt, function (err, hash) {
+                    new User({
+                        email: req.body.email,
+                        full_name: req.body.full_name,
+                        password: hash
+                    }).save()
+                    res.redirect('/login')
+                });
+            })
+        }
+
+    }else{
         res.redirect('/register?error=1')
-    }else if(req.body.password !== req.body.re_password){
-        res.redirect('/register?error=2')
     }
-    const findUser = await User.findOne({email: req.body.email}).count()
-    if(findUser){
-        res.redirect('/register?error=3')
-    }
-    bcrypt.genSalt(10, (err, salt) =>{
-        bcrypt.hash(req.body.password, salt, function(err, hash){
-            new User({
-                email: req.body.email,
-                full_name: req.body.full_name,
-                isAdmin: false,
-                password: hash,
-            }).save();
-            res.redirect('/login')
-        })
-    })
-};
+}
 
 const signIn = (req, res) => {
     res.redirect(`/profile/${req.user._id}`)
