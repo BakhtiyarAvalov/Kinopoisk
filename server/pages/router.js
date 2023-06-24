d9c4578eaa4e69530a12437e26373fc4aa58e5c5
BakhtiyarAvalov <Avalov90@mail.ru>
2023-06-24 12:49:17 +0600

    modernization of the profile page

--- server/pages/router.js
+++ server/pages/router.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router();
 const Genres = require('../Genres/Genres')
 const Country = require('../Country/Country')
+const User = require('../auth/user')
 
 router.get('/', async(req, res) =>{
     const allGenres = await Genres.find()
@@ -17,9 +18,15 @@ router.get('/register', (req, res)=>{
     res.render("register.ejs", {user: req.user ? req.user : {}})
 })
 
-router.get('/profile/:id', (req, res)=>{
-    res.render("profile.ejs", {user: req.user ? req.user : {}})
-})
+router.get('/profile/:id', async(req, res)=>{
+    const allGenres = await Genres.find();
+    const user = await User.findById(req.params.id)
+    if(user){
+        res.render("profile.ejs", {user: user, genres: allGenres, loginUser: req.user})
+    }else{
+        res.redirect('/not-found')
+    }
+});
 
 router.get('/admin', (req, res)=>{
     res.render("adminProfile.ejs", {user: req.user ? req.user : {}})
@@ -35,5 +42,9 @@ router.get('/edit', async(req, res)=>{
     const allGenres = await Genres.find()
     const getAllCountries = await Country.find()
     res.render("editFilm.ejs", {genres : allGenres, countries: getAllCountries, user: req.user ? req.user : {}})
+})
+
+router.get('/not-found', (req, res)=>{
+    res.render("notFound")
 })
 module.exports = router
