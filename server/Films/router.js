4b83ff7994917b680526ac9aba3c9974a3869c2b
BakhtiyarAvalov <Avalov90@mail.ru>
2023-06-28 21:33:07 +0600

    complated the logic of adding a file

--- server/Films/router.js
+++ server/Films/router.js
@@ -3,7 +3,8 @@ const express = require('express');
 const router = express.Router();
 const{upload} = require('./multer');
 const createFilm = require('./controller');
+const {isAuth} = require('../auth/middlewares')
 
-router.post('/api/new', createFilm);
+router.post('/api/new', isAuth, upload.single('image'), createFilm);
 
 module.exports = router;
